369297ab2538d11083a99103cd8f13341b9a78c9
MDRayaan <75481602+MDRayaan@users.noreply.github.com>
2021-05-25 16:57:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    
+    fill("white")
+    textSize(15)
+    text("N O N U T I C F ", 100,250)
+    fill("lightblue")
+    text("Hint: Performs A set of tasks !!", 100,265)
+    
+    fill("white")
+    textSize(15)
+    text("T D A A S B A E", 700,140)
+    fill("lightblue")
+    text("Hint: stores all information", 700,180)
+    
+
+}
